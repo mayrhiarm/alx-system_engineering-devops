87549b7d7669c2fd46f8d2bdd53773c8a650136f
mayrhiarm <mariamsanni93@gmail.com>
2024-03-22 15:36:40 -0700

    tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Install Flask 
+# Using Puppet, install flask from pip3
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
-  require  => Package['python3-pip'],
+  provider => 'pip3',
 }
